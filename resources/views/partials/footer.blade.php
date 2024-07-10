41fab5b19c2adadf15853d93dfc8d9f91fb4a023
Cyborg-44 <gospelhack953@gmail.com>
2024-07-10 16:52:06 +0100

    mise à jour

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="{{ asset('assets/css/partials/footer.css') }}">
 <footer id="footer" class="footer accent-background">
 
-    <div class="container-md footer-top mr-2">
+    <div class="container-md footer-bottom">
         <div class="row gy-4">
             <div class="col-lg-5 col-md-12 footer-about">
                 <a href="index.html" class="logo d-flex align-items-center">
@@ -11,10 +11,11 @@
                 <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies
                     darta donna mare fermentum iaculis eu non diam phasellus.</p>
                 <div class="social-links d-flex mt-4">
-                    <a href=""><i class="bi bi-twitter-x"></i></a>
-                    <a href=""><i class="bi bi-facebook"></i></a>
-                    <a href=""><i class="bi bi-instagram"></i></a>
-                    <a href=""><i class="bi bi-linkedin"></i></a>
+                    <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="#" ><i class="bi bi-youtube"></i></a>
+                <a href="#" ><i class="bi bi-telegram"></i></a>
+                <a href="#" ><i class="bi bi-instagram"></i></a>
+                <a href="#" ><i class="bi bi-linkedin"></i></a>
                 </div>
             </div>
 
@@ -32,11 +33,11 @@
             <div class="col-lg-2 col-6 footer-links">
                 <h4>Our Services</h4>
                 <ul>
-                    <li><a href="#">Web Design</a></li>
-                    <li><a href="#">Web Development</a></li>
-                    <li><a href="#">Product Management</a></li>
-                    <li><a href="#">Marketing</a></li>
-                    <li><a href="#">Graphic Design</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
                 </ul>
             </div>
 
@@ -45,7 +46,7 @@
                 <p>A108 Adam Street</p>
                 <p>New York, NY 535022</p>
                 <p>United States</p>
-                <p class="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
+                <p class="mt-4"><strong>Phone:</strong> <span>+000 000000</span></p>
                 <p><strong>Email:</strong> <span>info@example.com</span></p>
             </div>
 
