fb07294420a8016d838c4034416a22904ec1f5d3
Cyborg-44 <gospelhack953@gmail.com>
2024-07-11 14:45:40 +0100

    mise à jour

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,10 +12,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
+    </head>
 <body>
     {{-- Globla header  --}}
-    <header id="header" class="header fixed">
+    <header id="header" class="header fixed-top">
         @include('partials.header')
     </header>
 
@@ -30,5 +30,6 @@
     </footer>
     <!-- Inclure le JS compilé -->
     <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -5,6 +5,47 @@
 
 @section('content')
     <!-- Hero Section -->
-    
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('img/Bullion-Coins!.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/Silver-Opportunities-Web-Banner.jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/Website-Banner-1 (1).jpg') }}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/Website-Banner-2-(Smart-Bonanza) (1).jpg') }}" class="d-block w-100"
+                    alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('img/Website-Banner-3-(Smart-Plus) (1).jpg') }}" class="d-block w-100"
+                    alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <section id="hero" class="hero section accent-background">
+        <div class="container position-relative text-center" data-aos="fade-up" data-aos-delay="100">
+            <div class="row gy-5 justify-content-center">
+                <div>
+                    <h2><span>Welcome to </span><span class="accent">ADL</span></h2>
+                    <p>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+                </div>
+            </div>
+        </div>        
+      </section>
     <!-- /Hero Section -->
 @endsection
