ed66ca8a3b55387cf565cb85b8e117df8cf52aac
Cyborg-44 <gospelhack953@gmail.com>
2024-07-12 15:54:38 +0100

    mise a jour

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,35 +1,58 @@
-<!-- resources/views/layouts/app.blade.php  : Fichier de démarage de l'application -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ADL - Application</title>
-    
-    {{-- importation des fichiers de styles et des liens de style et d'icons  --}}
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-icons.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>App - Application</title>
+
+    <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  {{-- <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet"> --}}
+  <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+
+  {{-- STYLE CSS FILES  --}}
+  <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+</head>
+
 <body>
-    {{-- Globla header  --}}
-    <header id="header" class="header fixed-top">
-        @include('partials.header')
+    <header>
+
     </header>
-
     <main class="main">
-        {{-- Hero section  --}}
-            @yield('content')
-
+        {{-- HERO SECTION    --}}
+        @yield('content')
     </main>
+    
+    <footer>
 
-    <footer id="footer" class="footer accent-background">
-        {{-- @include('partials.footer') --}}
     </footer>
-    <!-- Inclure le JS compilé -->
+
+    <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  {{-- <div id="preloader"></div> --}}
+        {{-- Importation de tous les fichiers JS utilisés dans l'application  --}}
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+
+    {{-- importation du fichier de configuration JS de l'application  --}}
     <script src="{{ asset('assets/js/app.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,77 +1,60 @@
-{{-- Ceci est le fichier d'implémentation du header du site web;
-il est appelé dans le fichier layout.app  --}}
+<header id="header" class="header fixed-top">
 
-<link rel="stylesheet" href="{{ asset('assets/css/partials/header.css') }}">
-
-
-<div class="topbar d-flex align-items-center">
+  <div class="topbar d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
-        <div class="contact-info d-flex align-items-center">
-            <i class="bi bi-phone d-flex align-items-center "><span>+1 5589 55488 55</span></i>
-            <i class="bi bi-envelope d-flex align-items-center ms-4"><a
-                    href="mailto:contact@example.com">contact@example.com</a></i>
-        </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="twitter"><i class="bi bi-youtube"></i></a>
-            <a href="#" class="twitter"><i class="bi bi-telegram"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        </div>
-
-        <div class="form-info d-flex align-items-center">
-            <ul class="list-unstyled d-flex m-0">
-                <li class="m-3 pl-3">
-                    <a href="#" class="text-white d-flex align-items-center">
-                        <i class="bi bi-person-fill me-2 text-white"></i> Login
-                    </a>
-                </li>
-                <li class="m-3 pl-3">
-                    <a href="#" class="text-white d-flex align-items-center">
-                        <i class="bi bi-pencil-square me-2 text-white"></i>Register
-                    </a>
-                </li>
-            </ul>
-        </div>
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
     </div>
-</div><!-- End Top Bar -->
+  </div><!-- End Top Bar -->
 
-<div class="branding d-flex align-items-cente">
+  <div class="branding d-flex align-items-cente">
 
     <div class="container position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1 class="sitename">ADL</h1>
-            <span>.</span>
-        </a>
-        {{-- Implémentation du menu: n'ayant pas tous les élément, il faudra remplacer les termes "partie {1}" par les termes appropriés  --}}
-        <nav id="navmenu" class="navmenu">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <h1 class="sitename">ADL</h1>
+        <span>.</span>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="#hero" class="active">Home<br></a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
-                <li><a href="#hero" class="active">Home<br></a></li>
-                <li class="dropdown"> <a href="#"><span>About</span> <i
-                            class="bi bi-chevron-down toggle-dropdown"></i> </a>
-                    <ul>
-                        <li><a href="#aboutus">About Us</a></li>
-                        <li class="dropdown">
-                            <a href="#team"><span>Agents</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                            <ul>
-                                <li><a href="#">Partie 1</a></li>
-                                <li><a href="#">Partie 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#team">Contact Us</a></li>
-                        <li><a href="#pricing">Pricing policy</a></li>
-                        <li><a href="#refund">Refund policy</a></li>
-                        <li><a href="#gallery">Gallery</a></li>
-                    </ul>
-                </li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#blog">Blog</a></li>
-                <li><a href="#products">Products</a></li>
-                <li><a href="#team">Team</a></li>
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
             </ul>
-            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
+          </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
     </div>
-</div>
+
+  </div>
+
+</header>
