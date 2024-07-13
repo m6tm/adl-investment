1374338fdb28e42d9e4b71fdc1b3d59ad6bc6bee
m6tm <maboadaniel.55@gmail.com>
2024-07-13 13:26:27 +0100

    Installation de tailwind

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./resources/**/*.{js,ts,tsx,css,scss,blade.php}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
