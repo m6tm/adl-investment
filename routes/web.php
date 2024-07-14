67a817f94397e55ceb10b69774e881aec1d81f65
m6tm <maboadaniel.55@gmail.com>
2024-07-14 21:05:12 +0100

    Connexion - Inscription ajouté #1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SigninController;
+use App\Http\Controllers\SignUpController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 /*route d'appel du fichier de configuration de l'application */
 Route::get('/', function () {
     return view('layout.app');
@@ -27,9 +25,12 @@ Route::get('/home', function () {
     return view('pages.home.home');
 });
 
-Route::get('/', function () {
-    return view('pages.home.home');
-});
+// Route::get('/', function () {
+//     return view('pages.home.home');
+// });
+
+Route::get('/signin', [SigninController::class, 'index'])->name('signin');
+Route::get('/signup', [SignUpController::class, 'index'])->name('signup');
 
 Route::middleware(['auth.dashboard'])->group(function () {
 });
