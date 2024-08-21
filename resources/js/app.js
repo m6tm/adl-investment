a80a65f8069d27e2750a6b274af12a9b166914e9
m6tm <maboadaniel.55@gmail.com>
2024-08-21 15:00:00 +0100

    Correction des erreurs de traduction #1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,2 @@
-// import './bootstrap';
+import './bootstrap'
 import './utilities'
-    
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,15 +2,15 @@
 import 'bootstrap'
 import './templately/js/index'
 
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import { notificationTabs } from './utilities/notification';
-
 import axios from 'axios';
+import { notificationTabs } from './utilities/notification';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
