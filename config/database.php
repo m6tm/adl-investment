0cc1ddff35aaa4e31638a3c3060fca3d594ceaa7
m6tm <maboadaniel.55@gmail.com>
2024-08-22 10:54:45 +0100

    Les logs applicatives ont étés ajouté

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'activitylog_db_connection' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('ACTIVITY_LOGGER_DB_HOST', '127.0.0.1'),
+            'port' => env('ACTIVITY_LOGGER_DB_PORT', '3306'),
+            'database' => env('ACTIVITY_LOGGER_DB_DATABASE', 'forge'),
+            'username' => env('ACTIVITY_LOGGER_DB_USERNAME', 'forge'),
+            'password' => env('ACTIVITY_LOGGER_DB_PASSWORD', ''),
+            'unix_socket' => env('ACTIVITY_LOGGER_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
