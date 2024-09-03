b9d8f252641722d0d09dc413141ff8273c223496
J-Bacob <jbacob@afaya-it.com>
2024-09-03 10:33:29 +0100

    Ticket UIs done

--- resources/views/dashboard/pages/tickets/payment.blade.php
+++ resources/views/dashboard/pages/tickets/payment.blade.php
@@ -37,6 +37,7 @@
                         </div>
                     </div>
 
+                    <!-- STEP 1 -->
                     <div class="flex justify-around mt-20">
                         <a
                             href="#"
@@ -52,12 +53,90 @@
                         </a>
                     </div>
 
+                    <!-- STEP 2 -->
+                    <div class="grid grid-rows-4 place-content-center mt-20">
+                        <div class="mb-5.5">
+                            <div class="w-96">
+                                <label class="mb-3 block text-sm font-medium text-black dark:text-white" for="name">Nom</label>
+                                <div class="relative">
+                                    <span class="absolute left-4.5 top-3.5">
+                                        <i data-lucide="user" class="size-5"></i>
+                                    </span>
+                                    <input
+                                        class="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-slate-100 dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                        type="text" name="name" id="name" placeholder="Devid Jhon" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-5.5">
+                            <div class="w-96">
+                                <label class="mb-3 block text-sm font-medium text-black dark:text-white" for="first_name">Prénom</label>
+                                <div class="relative">
+                                    <span class="absolute left-4.5 top-3.5">
+                                        <i data-lucide="user" class="size-5"></i>
+                                    </span>
+                                    <input
+                                        class="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-slate-100 dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                        type="text" name="first_name" id="first_name" placeholder="Rene Mode" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-5.5">
+                            <div class="w-96">
+                                <label class="mb-3 block text-sm font-medium text-black dark:text-white" for="phone">Numéro de
+                                    téléphone</label>
+                                <div class="relative">
+                                    <span class="absolute left-4.5 top-3.5">
+                                        <i data-lucide="phone" class="size-5"></i>
+                                    </span>
+                                    <input
+                                        class="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-slate-100 dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                        type="tel" name="phone" id="phone" placeholder="+XXX XXXX XXXX" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-5.5">
+                            <div class="w-96">
+                                <label class="mb-3 block text-sm font-medium text-black dark:text-white" for="amount">Montant</label>
+                                <div class="relative">
+                                    <span class="absolute left-4.5 top-4">
+                                        <i data-lucide="dollar-sign" class="size-5"></i>
+                                    </span>
+                                    <input
+                                        class="w-full rounded border border-stroke bg-gray py-3 pl-11.5 pr-4.5 font-medium text-black focus:border-primary focus-visible:outline-none dark:border-slate-100 dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                                        type="pseudo" name="amount" id="amount" placeholder="23">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- STEP 3 -->
+                    <div class="grid justify-items-center mt-20">
+                        <div class="rounded-full ">
+                            <img src="{{ asset('assets/img/team/team-3.jpg') }}" class="rounded-full h-48 h-48" alt="User">
+                        </div>
+
+                        <div class="text-center mt-5">
+                            <p
+                                class="mb-4 text-title-sm2 font-medium leading-[30px] text-black dark:text-white md:text-2xl"
+                            >
+                                Status de la transaction
+                            </p>
+                            <p class="font-medium">
+                                Succès
+                            </p>
+                        </div>
+                    </div>
+
                     <div class="flex justify-between mt-20">
                         <a href="{{ route('dashboard.tickets.create') }}" class="px-4 py-2 inline-block rounded-md bg-primary text-white">
                             Modifier
                         </a>
                         <a href="{{ route('dashboard.tickets.pay') }}" class="px-4 py-2 ml-5 inline-block rounded-md bg-primary text-white">
                             Next
+                        </a>
+                        <a href="{{ route('dashboard.tickets') }}" class="px-4 py-2 ml-5 inline-block rounded-md bg-primary text-white">
+                            Terminer
                         </a>
                     </div>
                 </div>
