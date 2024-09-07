f59043c362da83d342dca9aba37d5e36f9f6b0e9
m6tm <maboadaniel.55@gmail.com>
2024-09-07 17:11:59 +0100

    Vérification de compte client #3

--- database/factories/TelephoneFactory.php
+++ database/factories/TelephoneFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Telephone;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Telephone>
@@ -25,9 +26,14 @@ class TelephoneFactory extends Factory
      */
     public function definition(): array
     {
+        $data_countries = Storage::disk('data')->get('CountryCodes.json');
+        $data_countries = json_decode($data_countries, true);
+        $countries = Telephone::all()->map(fn(Telephone $telephone) => $telephone->code)->toArray();
+        $data_countries = array_filter($data_countries, fn($country) => !in_array($country['code'], $countries));
+        $selected_country = $data_countries[array_rand($data_countries)];
         return [
             'user_id' => User::factory(),
-            'code' => $this->faker->countryCode(),
+            'code' => $selected_country['code'],
             'telephone' => $this->faker->unique()->phoneNumber(),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DocumentAutoriseSeeder::class,
             CountryDocumentAutorizedSeeder::class,
+            UserInformationFillSeeder::class,
         ]);
     }
 }
