0fd0f3ef8d1ce84a1cc252e7fc8f9c4c4df9446a
maboa daniel emmanuel <maboadaniel.55@gmail.com>
2024-09-20 16:03:52 +0000

    Merge branch 'm6tm' of https://github.com/m6tm/adl-investment into m6tm

--- resources/views/dashboard/pages/users/index.blade.php
+++ resources/views/dashboard/pages/users/index.blade.php
@@ -1,11 +1,11 @@
 @php
 	$title = __('users.title');
-    $breadcrumbs = [
+    $breadcrumb = [
         ['title' => $title],
     ];
 @endphp
 
-<x-dashboard.layout :title="$title" :breadcrumbs="$breadcrumbs">
+<x-dashboard.layout :title="$title" :breadcrumb="$breadcrumb">
     <div class="flex flex-col gap-5 md:gap-7 2xl:gap-10">
         <div class="border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark rounded-[10px] p-5">
 			<x-error-message-alert class="mb-4" />
