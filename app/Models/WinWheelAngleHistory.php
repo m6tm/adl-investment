f30452cea7b79fe9606ec37187e171a85393bdaf
m6tm <maboadaniel.55@gmail.com>
2024-10-12 14:32:46 +0100

    Ajout d'une table pour la liste des angles de départ

---
+++ app/Models/WinWheelAngleHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WinWheelAngleHistory extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_10_12_121923_create_win_wheel_angle_histories_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('win_wheel_angle_histories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('angle');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('win_wheel_angle_histories');
+    }
+};
